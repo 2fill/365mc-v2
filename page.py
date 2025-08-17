b23245d7c8cdddd1c5bc49cbe2f541ba1b3cf838
2fill <ichaeu@naver.com>
2025-08-17 23:34:04 +0900

    feat: 세 번째 페이지 모든 요소 추가

--- page.py
+++ page.py
@@ -364,7 +364,7 @@ elif st.session_state.page == "page2":
             <div style="position: relative; background-color: #FFF3EB; min-height: 60vh; border-radius: 8px; display: flex; flex-direction: column; align-items: flex-start; justify-content: center; font-size: 25px; font-weight: bold; color: rgba(0, 0, 0, 0.7); padding: 0; gap: 20px; margin-right: 70px;">
                 <div style="display: flex; align-items: center;">
                     <div style="background-color: #F36F20; color: FFFFFF; border-radius: 50%; width: 40px; height: 40px; display: flex; align-items: center; justify-content: center; font-size: 20px; margin: 50px 15px 50px 20px; flex-shrink: 0;">✔️</div>
-                    <span style="color: #777777;">Liposuction Information</span>
+                    <span style="color: rgba(0, 0, 0, 0.7);">Liposuction Information</span>
                 </div>
                 <div style="position: absolute; top: 138px; left: 40px; width: 3px; height: 110px; background-color: #F36F20;"></div>
                 <div style="display: flex; align-items: center;">
@@ -484,6 +484,204 @@ elif st.session_state.page == "page2":
             with col_next:
                 if st.button("Next", key="next_page2"):
                     st.session_state.page = "page3"
+
+            st.markdown('</div>', unsafe_allow_html=True)
+            st.markdown('</div>', unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+elif st.session_state.page == "page3":
+    # 로고
+    img_path = Path("assets/Logo.png")
+    print(img_path.exists())
+
+    def img_to_base64(img_path):
+        with open(img_path, "rb") as img_file:
+            b64_str = base64.b64encode(img_file.read()).decode()
+        return b64_str
+
+    img_b64 = img_to_base64(img_path)
+
+    # 마크다운
+    st.markdown(
+        f"""
+        <style>
+        
+        /* 배경 */
+        .stApp {{
+            background-color: #F36F20;
+        }}
+
+        /* 로고 */
+        .custom-logo {{
+            position: fixed;
+            top: 30px;
+            left: 50px;
+            width: 300px;
+        }}
+
+        /* 헤더 숨기기 */
+        header[data-testid="stHeader"] {{
+            display: none;
+        }}
+        
+        /* 메인 박스 */
+        section > div {{
+            height: 100% !important;
+            max-height: none !important;
+            width: 100% !important;
+            max-width: none !important;
+        }}
+
+        section > div > div {{
+            background-color: white !important;
+            padding: 50px !important;
+            border-radius: 10px !important;
+            box-shadow: 0 4px 12px rgba(0,0,0,0.15) !important;
+            height: 100% !important;
+            width: 85% !important;
+            margin: 80px auto 40px auto !important;
+            color: rgba(0, 0, 0, 0.7) !important;
+            position: relative;
+        }}
+        
+        /* 좌우 컬럼 부모 */
+        div[data-testid="stVerticalBlock"] > div {{
+            height: 100% !important;
+            display: flex !important;
+        }}
+        
+        input[type="number"] {{
+            background-color: #FFFFFF !important;
+            border-radius: 5px !important;
+            padding: 5px 10px !important;
+            box-shadow: none !important; 
+            height: 40px !important; 
+            width: 150px;
+            font-size: 20px !important;
+            color: rgba(0, 0, 0, 0.7);
+        }}
+        
+        /* 스핀 버튼 */
+        input[type=number]::-webkit-inner-spin-button,
+        input[type=number]::-webkit-outer-spin-button {{
+            -webkit-appearance: none !important;
+            margin: 0 !important;
+        }}
+        
+        .stButton>button {{
+            background-color: #E9E3E0 !important;  /* 회색 */
+            color: rgba(0, 0, 0, 0.7) !important;
+            border: none !important;
+            border-radius: 10px !important;
+            cursor: pointer !important;
+            transition: background-color 0.3s ease !important;
+            width: 250px;
+        }}
+
+        /* 클릭 시 주황색 */
+        .stButton>button:active {{
+            background-color: #F36F20 !important; 
+            color: white !important;
+        }}
+        
+        </style>
+
+
+
+        <img src="data:image/png;base64,{img_b64}" class="custom-logo" />
+        """,
+        unsafe_allow_html=True
+        )
+        
+    with st.container():
+        # 좌우 박스 생성
+        left_col, right_col = st.columns([1, 3])  # 비율
+
+        with left_col:
+            st.markdown('''
+            <div style="position: relative; background-color: #FFF3EB; min-height: 60vh; border-radius: 8px; display: flex; flex-direction: column; align-items: flex-start; justify-content: center; font-size: 25px; font-weight: bold; color: rgba(0, 0, 0, 0.7); padding: 0; gap: 20px; margin-right: 70px;">
+                <div style="display: flex; align-items: center;">
+                    <div style="background-color: #F36F20; color: FFFFFF; border-radius: 50%; width: 40px; height: 40px; display: flex; align-items: center; justify-content: center; font-size: 20px; margin: 50px 15px 50px 20px; flex-shrink: 0;">✔️</div>
+                    <span style="color: rgba(0, 0, 0, 0.7);">Liposuction Information</span>
+                </div>
+                <div style="position: absolute; top: 138px; left: 40px; width: 3px; height: 110px; background-color: #F36F20;"></div>
+                <div style="display: flex; align-items: center;">
+                    <div style="background-color: #F36F20; color: rgba(0, 0, 0, 0.7); border-radius: 50%; width: 40px; height: 40px; display: flex; align-items: center; justify-content: center; font-size: 20px; margin: 50px 15px 50px 20px; flex-shrink: 0;">✔️</div>
+                    <span style="color: rgba(0, 0, 0, 0.7);">Liposuction Information</span>
+                </div>
+                <div style="position: absolute; top: 298px; left: 40px; width: 3px; height: 110px; background-color: #F36F20;"></div>
+                <div style="display: flex; align-items: center;">
+                    <div style="background-color: rgba(138, 138, 138, 0.2); color: #777777; border: 3px solid #F36F20; border-radius: 50%; width: 40px; height: 40px; display: flex; align-items: center; justify-content: center; font-size: 20px; margin: 50px 15px 50px 20px; flex-shrink: 0;">3</div>
+                    <span style="color: rgba(0, 0, 0, 0.7);">Body Composition</span>
+                </div>
+            </div>
+            ''', unsafe_allow_html=True)
+
+
+
+
+
+        with right_col:
+            st.markdown(
+            """
+            <div style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; margin: 0; padding: 0; position: relative;">
+            """, unsafe_allow_html=True)
+
+            st.markdown(
+                """
+                <div style="display: flex; gap: 30px; margin-bottom: 15px;">
+                """, unsafe_allow_html=True
+            )
+
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                height = st.number_input("Height", min_value=0, max_value=300, key="height")
+            with col2:
+                weight = st.number_input("Weight", min_value=0, max_value=500, key="weight")
+            with col3:
+                bmi = st.number_input("BMI", min_value=0.0, max_value=100.0, key="bmi")
+
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                smm = st.number_input("Skeletal muscle mass", min_value=0.0, max_value=200.0, key="smm")
+            with col2:
+                bfm = st.number_input("Body fat mass", min_value=0.0, max_value=200.0, key="bfm")
+            with col3:
+                tbw = st.number_input("Total body water", min_value=0.0, max_value=200.0, key="tbw")
+
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                ffm = st.number_input("Fat-free mass", min_value=0.0, max_value=200.0, key="ffm")
+            with col2:
+                bp = st.number_input("Body protein", min_value=0.0, max_value=200.0, key="bp")
+            with col3:
+                bm = st.number_input("Body mineral", min_value=0.0, max_value=200.0, key="bm")
+
+            col1 = st.columns(1)[0]
+            with col1:
+                whr = st.number_input("Waist-hip ratio", min_value=0.0, max_value=5.0, key="whr")
+
+            st.markdown("</div>", unsafe_allow_html=True)
+            
+            st.markdown('<div style="display: flex; justify-content: flex-end; gap: 20px;">', unsafe_allow_html=True)
+
+            col_back, col_next = st.columns([1, 1])
+
+            with col_back:
+                if st.button("Back", key="back_page3"):
+                    st.session_state.page = "page2"
+
+            with col_next:
+                if st.button("Complete", key="next_page3"):
+                    st.session_state.page = "page4"
 
             st.markdown('</div>', unsafe_allow_html=True)
             st.markdown('</div>', unsafe_allow_html=True)
